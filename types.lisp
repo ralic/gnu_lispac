f5adc71d031e9fce8b1990e8e0eb84d7df43a0ab
sorancio <sorancio@gmail.com>
2010-05-28 21:54:26 +0200

    Added License and optimized types.lisp

--- types.lisp
+++ types.lisp
@@ -1,3 +1,22 @@
+;; types.lisp
+;;
+;; Copyrigth (C) 2010 Kevin Mas Ruiz <sorancio>
+;;
+;; This file is part of lispac.
+;;
+;; lispac is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; lispac is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with lispac.  If not, see <http://www.gnu.org/licenses/>.
+
 (in-package lispac)
 
 (defmacro b-or (&rest bits)
@@ -11,12 +30,12 @@
 (defconstant +horitzontal-connection+ 1)
 (defconstant +vertical-connection+ 2)
 (defconstant +full-connection+ 3) ; (b-or 1 2)
-
-;; Surface constants (they are shared and read-only)
+;; Wall surfaces
 (defconstant +top-wall-surface+ nil)
 (defconstant +left-wall-surface+ nil)
 (defconstant +bottom-wall-surface+ nil)
 (defconstant +right-wall-surface+ nil)
+; Point surfaces
 (defconstant +bronze-coin-surface+ nil)
 (defconstant +silver-coin-surface+ nil)
 (defconstant +golden-coin-surface+ nil)
@@ -28,6 +47,7 @@
 (defconstant +bit-0-surface+ nil)
 (defconstant +bit-1-surface+ nil)
 (defconstant +diamond-surface+ nil)
+;; Player surfaces
 (defconstant +player-left-surface+ nil)
 (defconstant +player-left-moving-surface+ nil)
 (defconstant +player-up-surface+ nil)
@@ -37,7 +57,35 @@
 (defconstant +player-down-surface+ nil)
 (defconstant +player-down-moving-surface+ nil)
 (defconstant +player-life-surface+ nil)
-
+;; Weak state monster surfaces
+(defconstant +weak-monster-left-surface+ nil)
+(defconstant +weak-monster-left-moving-surface+ nil)
+(defconstant +weak-monster-up-surface+ nil)
+(defconstant +weak-monster-up-moving-surface+ nil)
+(defconstant +weak-monster-right-surface+ nil)
+(defconstant +weak-monster-right-moving-surface+ nil)
+(defconstant +weak-monster-down-surface+ nil)
+(defconstant +weak-monster-down-moving-surface+ nil)
+;; Normal state monster surfaces
+(defconstant +monster-left-surface+ nil)
+(defconstant +monster-left-moving-surface+ nil)
+(defconstant +monster-up-surface+ nil)
+(defconstant +monster-up-moving-surface+ nil)
+(defconstant +monster-right-surface+ nil)
+(defconstant +monster-right-moving-surface+ nil)
+(defconstant +monster-down-surface+ nil)
+(defconstant +monster-down-moving-surface+ nil)
+;; Angry state monster surfaces
+(defconstant +angry-monster-left-surface+ nil)
+(defconstant +angry-monster-left-moving-surface+ nil)
+(defconstant +angry-monster-up-surface+ nil)
+(defconstant +angry-monster-up-moving-surface+ nil)
+(defconstant +angry-monster-right-surface+ nil)
+(defconstant +angry-monster-right-moving-surface+ nil)
+(defconstant +angry-monster-down-surface+ nil)
+(defconstant +angry-monster-down-moving-surface+ nil)
+;; Other surfaces
+(defconstant +life-surface+ nil)
 ;; Point value constants
 (defconstant +bronze-coin+ 5)
 (defconstant +silver-coin+ 10)
@@ -51,35 +99,54 @@
 (defconstant +bit-1+ 2560)
 (defconstant +diamond+ 5120)
 
-;; Monster state
-(defconstant +weak+ 1) ;; Can be killed and moves slower
-(defconstant +normal+ 2) ;; Normal state, can't be killed
-(defconstant +angry+ 3) ;; Can't be killed and moves faster
-
 (defstruct position 
   x 
   y)
 
-(defclass tile () ;; Tiles should inherits object?
-  ((surface :accessor set-surface :reader get-surface :initform nil)
-   (position :accessor set-position :reader get-position 
-             :initform (make-position :x 0 :y 0))
-   (flags :accessor set-flags :reader get-flags :initform +no-connection+)))
+(defclass tile () ;; Tile class should inherits object class?
+  ((surface :initform nil)
+   (position :initform (make-position :x 0 :y 0))
+   (flags :initform +no-connection+)))
 
 (defclass object ()
-  (position :accessor set-position :reader get-position))
+  (position))
 
 (defclass life (object)
-  (count :accessor set-count :reader get-count :initform 1))
+  (count :initform 1))
 
 (defclass point (object)
-  (type :accessor set-type :reader get-type :initform +bronze-coin+))
+  (type :initform +bronze-coin+))
 
-(defclass player (object)
-  (points :accessor set-points :reader get-points :initform 0)
-  (lifes :accesor set-lifes :reader get-lifes :initform 1))
+(defclass creature (object)
+  (direction :initform :left))
 
-(defclass monster (object)
-  (state :accessor get-state :reader set-state :initform +normal+))
+(defclass player (creature)
+  (points :initform 0)
+  (lifes :initform 1))
 
-(defmethod get-object-surface)
+(defclass monster (creature)
+  (state :initform :normal))
+
+;; get-object-surface method
+;;; Returns the symbol of the surface
+
+(defmethod get-object-surface (object object)
+  nil)
+
+(defmethod get-object-surface (life life)
+  '+life-surface+)
+
+(defmethod get-object-surface (point pointobj)
+  (case (type pointobj)
+    ((+bronze-coin+) '+bronze-coin-surface+)
+    ((+silver-coin+) '+silver-coin-surface+)
+    ((+golden-coin+) '+golden-coin-surface+)
+    ((+cherry+) '+cherry-surface+)
+    ((+banana+) '+banana-surface+)
+    ((+melon+) '+melon-surface+)
+    ((+water-melon+) '+water-melon-surface+)
+    ((+golden-cherry+) '+golden-cherry-surface+)
+    ((+bit-0+) '+bit-0-surface+)
+    ((+bit-1+) '+bit-1-surface+)
+    ((+diamond+) '+diamond-surface+)
+    (otherwise nil)))
